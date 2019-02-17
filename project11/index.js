dfadb11f74c532236e9bd4ba50e94bb8baa0b185
ermondel <ermondel@gmail.com>
2019-02-17 13:33:53 +0200

    feat: add another test pack

---
+++ project11/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import { str1, str2, str3 } from './lib';
+
+document.getElementById('app').innerHTML = `<p>${str1()}</p> <p>${str2()}</p> <p>${str3()}</p>`;
+
+console.log('---', DEV_MODE, FOO)
---
+++ project11/webpack.config.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const webpack = require('webpack');
+
+const DEV_MODE = (
+    !process.env.NODE_ENV || 
+    process.env.NODE_ENV === 'development' ? true : false
+);
+
+module.exports = {
+    entry: './index',
+    output: {
+        filename: 'build.js'
+    },
+    watch: DEV_MODE,
+    devtool: DEV_MODE ? "source-map" : undefined,
+    plugins: [
+        new webpack.DefinePlugin({
+            DEV_MODE: DEV_MODE,
+            FOO: JSON.stringify('BAR')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
