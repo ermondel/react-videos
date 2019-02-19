99c28f34a97232e17cfe27e2b76393a84f50514d
ermondel <ermondel@gmail.com>
2019-02-19 21:36:40 +0200

    feat: add another test pack

---
+++ project12/lib.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const str1 = () => 'First string';
+const str2 = () => 'Second string';
+const str3 = () => 'Third string';
+const str4 = () => 'Fourth string';
+
+export { str1, str2, str3, str4 }
