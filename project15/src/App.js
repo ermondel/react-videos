94d82178ba9d3f37d29da679d8e0308183d6bc87
ermondel <ermondel@gmail.com>
2019-03-01 15:29:13 +0200

    feat: add pr15

---
+++ project15/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => (
+    <div className="App">
+        <p>example 1</p>
+        <p>example 2</p>
+        <p>example 3</p>
+        <p>example 4</p>
+        <p>example 5</p>
+    </div>
+);
+
+export default App;
