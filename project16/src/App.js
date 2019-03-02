e7720f5c4b05529d4fc23c4f64c86faa4b5cd933
ermondel <ermondel@gmail.com>
2019-03-02 22:26:48 +0200

    feat: add pr16

---
+++ project16/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => (
+    <div className="app">
+        <p>success 1</p>
+        <p>success 2</p>
+        <p>success 3</p>
+        <p>success 4</p>
+        <p>success 5</p>
+        <p>success 6</p>
+    </div>
+);
+
+export default App;
