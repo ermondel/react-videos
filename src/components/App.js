2d04d812b018a484c9ddb5112d3bff714a672ef6
Ermondel <ermondel@gmail.com>
2020-02-14 17:46:49 +0200

    feat: provide query to youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const resposne = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: resposne.data.items });
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <p>I have {this.state.videos.length} videos.</p>
+      </div>
+    );
   }
 }
 
